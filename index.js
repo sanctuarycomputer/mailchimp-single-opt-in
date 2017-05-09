cc209ea8be9c08a1e2d5287e3bf8cd043bf9b175
Hugh Francis <me@hughfrancis.me>
2017-05-09 19:53:13 -0400

    Setup GET Request

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
-server.connection({ port: 3000, host: 'localhost' });
+server.connection({ port: (process.env.PORT || 3000), host: '0.0.0.0' });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Mailchimp Single Opt In is running on this URL. Send a POST request with { email: "hello@example.com" } to create subscribers.');
+  }
+});
 
 server.route({
   method: 'POST',
