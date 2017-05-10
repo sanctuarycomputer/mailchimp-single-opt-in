bd44b472f053a54bc11d5405dcf4c036982ee88b
Hugh Francis <me@hughfrancis.me>
2017-05-09 21:39:23 -0400

    Nicer Detail Message

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.route({
   method: 'POST',
   path: '/',
   handler: function (request, reply) {
-    if (!request.payload.email) return reply({ detail: "no_email" }).code(400);
+    if (!request.payload.email) return reply({ detail: "Please provide a valid email address." }).code(400);
     if (!request.payload.list_id) return reply({ detail: "no_list_id" }).code(400);
     mailchimp.post(`/lists/${request.payload.list_id}/members`, {
       email_address: request.payload.email,
