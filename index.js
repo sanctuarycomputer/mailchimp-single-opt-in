cd6f3d1f66e4f3eda834cbec8b56c78e62cd6beb
Hugh Francis <me@hughfrancis.me>
2017-05-09 20:23:46 -0400

    Implement Mailchimp Stuff

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
-const Hapi = require('hapi');
-const server = new Hapi.Server();
+const Hapi      = require('hapi');
+const Mailchimp = require('mailchimp-api-v3');
+const server    = new Hapi.Server();
 server.connection({ port: (process.env.PORT || 3000), host: '0.0.0.0' });
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY);
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply('Mailchimp Single Opt In is running on this URL. Send a POST request with { email: "hello@example.com" } to create subscribers.');
+    if (!process.env.MAILCHIMP_API_KEY) {
+      reply('Mailchimp Single Opt In is missing the Mailchimp API KEY. Please ensure MAILCHIMP_API_KEY is defined - you can get one here: https://admin.mailchimp.com/account/api/.');
+    } else {
+      reply('Mailchimp Single Opt In is running on this URL. Send a POST request with { email: "hello@example.com", list_id: "123abc" } to create subscribers.');
+    }
   }
 });
 
@@ -16,7 +22,18 @@ server.route({
   method: 'POST',
   path: '/',
   handler: function (request, reply) {
-    reply('Hello, world!');
+    if (!request.payload.email) return reply({ status: "no_email" }).code(400);
+    if (!request.payload.list_id) return reply({ status: "no_list_id" }).code(400);
+    mailchimp.post(`/lists/${request.payload.list_id}/members`, {
+      email_address: request.payload.email,
+      status: 'subscribed'
+    }, function(err, results) {
+      if (err) {
+        const { type, title, detail, status } = err;
+        return reply({ type, title, detail, status }).code(status);
+      };
+      return reply({ status: "ok" }).code(200);
+    });
   }
 });
 
